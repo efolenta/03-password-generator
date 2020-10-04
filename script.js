c28fb0dba8a79c15fcc0f63f0632c510b1c4836c
Eric Folenta <ericfolenta@gmail.com>
2020-10-04 16:55:08 -0700

    Added variables for the numbers and characters

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var lengthEl = document.querySelector('#length');
-var uppercaseEl = document.querySelector('#uppercase');
-var lowercaseEl = document.querySelector('#lowercase');
-var numbersEl = document.querySelector('#numbers');
-var symbolsEl = document.querySelector('#symbols');
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 
 // Write password to the #password input
 function writePassword() {
